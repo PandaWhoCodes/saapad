07a2b44621654700dd73b5042706fb833f1b4a24
Thomas Ashish Cherian <ufoundashish@gmail.com>
2022-03-28 00:01:46 +0530

    add save data to gsheets

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
 from os import environ as env
 from flask import Flask, render_template, session, url_for, redirect
-from flask.json import JSONEncoder
 from datetime import datetime
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 import requests
 from urllib.parse import quote_plus, urlencode
+import gspread
 
 app = Flask(__name__)
 
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        try:
-            if isinstance(obj, datetime):
-                return obj.isoformat()
-            iterable = iter(obj)
-        except TypeError:
-            pass
-        else:
-            return list(iterable)
-        return JSONEncoder.default(self, obj)
-
-
 app = Flask(__name__, template_folder="templates", static_folder="templates/static")
-app.json_encoder = CustomJSONEncoder
 app.secret_key = "secret"
 oauth = OAuth(app)
 
 ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
 PROFILE_KEY = env.get("user", "user")
 JWT_PAYLOAD = env.get("jwt-payload", "JWT_PAYLOAD")
 COMPANY_DOMAIN = env.get("company_domain")
 MAX_MEALS_PER_DAY = int(env.get("MAX_MEALS_PER_DAY", 1))
+GOOGLE_APP_SCRIPT_URL = env.get("GOOGLE_APP_SCRIPT_URL")
 
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
+SPREAD_SHEET_ID = (
+    "1Q4FYsdfpQwNZar0GdNHuvALlfKWrsAgxiRFda9mXu78"  # Please set the Spreadsheet ID.
+)
+SHEET_NAME = "Sheet1"  # Please set the sheet name.
+
+gc = gspread.service_account()
+sh = gc.open_by_key(SPREAD_SHEET_ID)
+
 
 oauth.register(
     "auth0",
@@ -58,6 +54,30 @@ def home_page():
     return render_template("home.html")
 
 
+def add_sheet_data():
+    # https://ashish.ch/using-google-sheets-as-a-makeshift-database/
+    # TODO: make this cleaner
+    data = {
+        "Name": session[JWT_PAYLOAD]["name"],
+        "email": session[JWT_PAYLOAD]["email"],
+    }
+    sh.values_append(
+        SHEET_NAME,
+        {"valueInputOption": "USER_ENTERED"},
+        {
+            "values": [
+                [
+                    data["Name"],
+                    data["email"],
+                    str(session["todays_meals"]),
+                    str(datetime.now()),
+                ]
+            ]
+        },
+    )
+    return "okay"
+
+
 @app.route("/eat_now")
 def eat_now():
     if PROFILE_KEY not in session:
@@ -77,7 +97,9 @@ def eat_now():
     else:
         session["last_eat_date"] = str(datetime.now())
         session["todays_meals"] = 1
-    # print(session)
+    res = add_sheet_data()
+    # print(res.json())
+    print(res)
     day = datetime.now().strftime("%A")
     time = datetime.now().strftime("%H:%M %p")
     return render_template("eat_now.html", day=day, time=time)
